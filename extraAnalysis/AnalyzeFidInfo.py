dda46674c41059cb77dfc97e1a34f47e029a79f5
hehe <he.he@cern.ch>
2023-03-09 03:22:57 +0100

    Update Fiducial info plotting script

--- extraAnalysis/AnalyzeFidInfo.py
+++ extraAnalysis/AnalyzeFidInfo.py
@@ -1,9 +1,28 @@
 import ROOT as r
 import os,pdb,json,subprocess
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-y", "--year", dest="year",
+				  help="which year,16,17,18,tot")
+(options, args) = parser.parse_args()
+
+year = options.year
+
+if year =="16":
+    lumitext = "36.3"
+if year == "17":
+    lumitext = "41.5"
+if year == "18":
+    lumitext = "59.7"
+if year == "tot":
+    lumitext = "137.6"
 
 def countXY(h,hrsp,xy): #value of xy: "x" or "y"
     h2 = h.Clone(h.GetName()+"Count"+xy)
     nbins = h.GetNbinsX()
+    assert nbins == hrsp.GetNbinsX()
+    assert nbins == hrsp.GetNbinsY() 
     for i in range(1,nbins+1):
         if xy == "x":
             sumbin = sum([hrsp.GetBinContent(i,j) for j in range(1,nbins+1)])
@@ -16,7 +35,7 @@ def countXY(h,hrsp,xy): #value of xy: "x" or "y"
     return h2
 
 def getLumiTextBox():
-    texS = r.TLatex(0.65,0.96, str(59.7)+" fb^{-1} (13 TeV)")
+    texS = r.TLatex(0.65,0.96, lumitext+" fb^{-1} (13 TeV)")
     texS.SetNDC()
     texS.SetTextFont(42)
     texS.SetTextSize(0.045)
@@ -71,15 +90,15 @@ def redrawXaxis(h,varName):
     return xaxis
 
 
-def titleAndRatio(t,ra):
-    tex = r.TLatex(0.43,0.9,t)
+def titleAndRatio(t,ra,rpos):
+    tex = r.TLatex(0.4,0.9,t)
     tex.SetNDC()
     tex.SetTextFont(52)
     tex.SetTextColor(r.kBlue)
     tex.SetTextSize(0.035)
     tex.Draw()
 
-    rtex = r.TLatex(0.52,0.73,ra)
+    rtex = r.TLatex(rpos,0.73,ra)
     rtex.SetNDC()
     rtex.SetTextFont(52)
     rtex.SetTextColor(r.kBlue)
@@ -93,15 +112,15 @@ def plotHist(h1,h2,k1,k2):
 
     h1.Draw("HIST")
     h2.Draw("HIST SAME")
-    legend = r.TLegend (0.5 ,0.75 ,0.9 ,0.90)
+    legend = r.TLegend (0.6 ,0.75 ,0.9 ,0.85)
     #legend = r.TLegend (0.5 ,0.9 ,0.9 ,1.2)
-
-    legend.SetFillColor(0)
+    legend.SetBorderSize(1)
+    legend.SetFillColor(r.kWhite)
     #legend.SetBorderSize(2)
     legend.AddEntry(h1,k1,"l")
     legend.AddEntry(h2,k2,"l")
     legend.SetTextSize(0.03)
-    legend.SetLineWidth (0)
+    #legend.SetLineWidth (0)
     legend.Draw("same")
     portion = h2.Integral(1,h2.GetNbinsX())/h1.Integral(1,h1.GetNbinsX())
     return portion,legend
@@ -110,7 +129,8 @@ def plotHist(h1,h2,k1,k2):
 varstr="nJets mjj dEtajj jetPt[0] jetPt[1] absjetEta[0] absjetEta[1] MassAllj Mass0j Mass1j Mass2j Mass34j MassFull Mass0jFull Mass1jFull Mass2jFull Mass34jFull"
 vars = varstr.split(" ")
 #vars = ["MassFull"]
-outdir = "2018FidPlots"
+
+outdir = "%sFidPlots"%year
 pdfcommand=['convert']
 pdfcommand2=['convert']
 
@@ -126,7 +146,7 @@ for key in myvar_dict.keys(): #key is the variable
     units[key] = myvar_dict[key]["units"]
     prettyVars[key] = myvar_dict[key]["prettyVars"]
 
-fin = r.TFile("Fidcombined.root")
+fin = r.TFile(os.path.join(year,"Fidcombined.root"))
 channels = ["eeee","eemm","mmmm"]
 dict = {}
 for var in vars:
@@ -154,7 +174,7 @@ c1.cd()
 lineStyles = [1,2,1,2]
 colors = [r.kOrange,2,r.kOrange,2]
 for var in vars:
-    for chan in channels + ["Total"]:
+    for chan in ["Total"]+channels:
         hR,hT,hRsp = dict[var][chan]
         hdx = countXY(hR,hRsp,"x")
         hdy = countXY(hT,hRsp,"y")
@@ -173,39 +193,47 @@ for var in vars:
             h.SetLineColor(colors[i])
             h.GetXaxis().SetLabelSize(0)
             h.GetXaxis().SetTickLength(0)
+            h.SetLineWidth(4*h.GetLineWidth())
 
-        if "Full" in var:
-            c1.SetLogx()
-        else:
-            c1.SetLogx(0)        
+              
         c1.Divide(2,1)
 
         c1.cd(1)
+        if "Full" in var:
+            r.gPad.SetLogx()
+        else:
+            r.gPad.SetLogx(0)  
+
         portionR,legend1 = plotHist(hR,hdx,"Total signal","Out of fiducial")
         t1,t2,t3 = getLumiTextBox()
-        tex1,ratio1 = titleAndRatio("RECO Events","Out of fiducial portion %s"%(round(portionR,3)))
+        tex1,ratio1 = titleAndRatio("RECO Events %s"%chan,"Out of fiducial portion %s"%(round(portionR,3)),0.58)
         xa1 = redrawXaxis(hdx,var)
         #pdb.set_trace()
         
         c1.cd(2)
+        if "Full" in var:
+            r.gPad.SetLogx()
+        else:
+            r.gPad.SetLogx(0)  
+
         portionT,legend2 = plotHist(hT,hdy,"Total signal","Not reconstructed")
         t4,t5,t6 = getLumiTextBox()
-        tex2,ratio2 = titleAndRatio("Truth Events","Non-reconstructed portion %s"%(round(portionT,3)))
+        tex2,ratio2 = titleAndRatio("Truth Events %s"%chan,"Non-reconstructed portion %s"%(round(portionT,3)),0.52)
         xa2 = redrawXaxis(hdy,var)
         
         c1.SaveAs(os.path.join(outdir,"%s_%s.png"%(var,chan)))
 
-        if chan == "Total":
-            pdfcommand.append(os.path.join(outdir,"%s_%s.png"%(var,chan)))
-        else:
-            pdfcommand2.append(os.path.join(outdir,"%s_%s.png"%(var,chan)))
+        #if chan == "Total":
+        pdfcommand.append(os.path.join(outdir,"%s_%s.png"%(var,chan)))
+        #else:
+        #    pdfcommand2.append(os.path.join(outdir,"%s_%s.png"%(var,chan)))
 
         c1.Clear()
 
-pdfcommand.append(os.path.join(outdir,"total.pdf"))      
-pdfcommand2.append(os.path.join(outdir,"channels.pdf"))   
+pdfcommand.append(os.path.join("./","total_yr%s.pdf"%year))      
+#pdfcommand2.append(os.path.join(outdir,"channels.pdf"))   
 subprocess.call(pdfcommand)
-subprocess.call(pdfcommand2)
+#subprocess.call(pdfcommand2)
 
 
 
