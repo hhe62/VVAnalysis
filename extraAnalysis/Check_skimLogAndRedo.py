6de3956867ffed606e3a51a5b6075a7355b0ad9a
hehe <he.he@cern.ch>
2023-05-31 07:41:04 +0200

    Update script for checking skim log and setting up resubmission

--- extraAnalysis/Check_skimLogAndRedo.py
+++ extraAnalysis/Check_skimLogAndRedo.py
@@ -1,5 +1,11 @@
-import os, sys, json
+import os
+import sys
+import json
 
+#Usage: python Check_skimLogAndRedo.py folder year json_file
+
+common_err = "WARNING: In non-interactive mode release checks e.g. deprecated releases, production architectures are disabled."
+#Dir must be the root /nfs_scratch/<user>/<farmout_job_folder> for correct identification of folders
 dir = sys.argv[1]
 redolist = []
 for root,dirs,files in os.walk(dir):
@@ -9,27 +15,34 @@ for root,dirs,files in os.walk(dir):
             text = []
             with open(path) as ferr:
                 for line in ferr:
-                    text.append(line.strip())
-            if len(text) >1:
+                    text.append(line.rstrip())
+            if (not text[0] in common_err) or len(text) >1:
                 print("\nsomething happening with %s"%path)
                 redolist.append(path.split("/")[1])
+                print("Error Log:")
+                print("")
                 for item in text:
                     print(item)
 
+#Redo the whole dataset if one file has issue
 redolist = list(set(redolist))
 print("Redo needed:")
 print(redolist)
 
 year = sys.argv[2]
-with open("/hdfs/store/user/hhe62/rmfailedfolders.sh","w") as frm:
+#should end with .json
+json_name = sys.argv[3]
+rm_name = "cleanFailed_"+json_name.replace(".json",".sh")
+
+with open("/hdfs/store/user/hhe62/%s"%rm_name,"w") as frm:
     for item in redolist:
-        frm.write("rm -r %s/\n"%item)
+        frm.write("rm -r %s\n"%item)
 
-os.system("chmod u+x /hdfs/store/user/hhe62/rmfailedfolders.sh")
-print("/hdfs/store/user/hhe62/rmfailedfolders.sh created")
+os.system("chmod u+x /hdfs/store/user/hhe62/%s"%rm_name)
+print("/hdfs/store/user/hhe62/%s created"%rm_name)
 
-with open("/hdfs/store/user/hehe/newNtuples%s.json"%year) as json_file:
-  obj = json.load(json_file)
+with open("/hdfs/store/user/hehe/%s"%json_name) as json_file:
+    obj = json.load(json_file)
 
 #remove dataset that doesn't need resubmit from josn file 
 match = False
@@ -42,10 +55,10 @@ for key in obj.keys():
     if not match:
         del obj[key]
 
-with open("/hdfs/store/user/hehe/newNtuples%sResubmit.json"%year,'w') as output_file:
-        json.dump(obj,output_file,indent=4)
+with open("/hdfs/store/user/hehe/Resubmit_%s"%json_name,'w') as output_file:
+    json.dump(obj,output_file,indent=4)
 
-os.system("mv /hdfs/store/user/hehe/newNtuples%sResubmit.json ~/vvanalysis_skim/CMSSW_10_3_1/src/Data_manager/ZZ4lRun2DatasetManager/FileInfo/ZZ4l%s/ntuples.json"%(year,year))
+os.system("mv /hdfs/store/user/hehe/Resubmit_%s ~/vvanalysis_skim/CMSSW_10_3_1/src/Data_manager/ZZ4lRun2DatasetManager/FileInfo/ZZ4l%s/ntuples.json"%(json_name,year))
 print("New json moved to ~/vvanalysis_skim/CMSSW_10_3_1/src/Data_manager/ZZ4lRun2DatasetManager/FileInfo/ZZ4l%s/ntuples.json"%year)
         
 
