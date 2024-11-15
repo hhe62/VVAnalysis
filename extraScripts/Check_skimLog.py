259c72e50b8fb00ad927746ff58a9306190fdb42
hehe <he.he@cern.ch>
2024-11-15 07:08:08 +0100

    Some updates to skimming log checking scripts

--- extraScripts/Check_skimLog.py
+++ extraScripts/Check_skimLog.py
@@ -1,6 +1,12 @@
-import os,sys
+import os
+import sys
 
+#Usage: python Check_skimLogAndRedo.py folder 
+
+common_err = "WARNING: In non-interactive mode release checks e.g. deprecated releases, production architectures are disabled."
+#dir must be the root /nfs_scratch/<user>/<farmout_job_folder> for correct identification of folders
 dir = sys.argv[1]
+redolist = []
 for root,dirs,files in os.walk(dir):
     for name in files:
         if "skim-" in name and ".err" in name:
@@ -8,8 +14,11 @@ for root,dirs,files in os.walk(dir):
             text = []
             with open(path) as ferr:
                 for line in ferr:
-                    text.append(line.strip())
-            if len(text) >1:
-                print("something happening with %s"%path)
+                    text.append(line.rstrip())
+            if (not text[0] in common_err) or len(text) >1:
+                print("\nsomething happening with %s"%path)
+                redolist.append(path.split("/")[1])
+                print("Error Log:")
+                print("")
                 for item in text:
                     print(item)
